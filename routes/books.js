2cdbbb45242039ed04ea1888f50a9a2ba7f8c26d
Snir Holland <sniro84@hotmail.com>
2019-09-01 09:47:36 +0300

    fixed update

--- routes/books.js
+++ routes/books.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../config/database');
+// const db = require('../config/database');
 const Book = require('../models/Book');
 
 // Books list
 router.get('/', (req,res) => Book.findAll()
-    .then(books => {
-        res.render('index.pug' , {
+    .then( (books) => {
+        res.render('index' , {
             books
         });
     })
@@ -53,5 +53,55 @@ router.post('/new', (req,res) => {
     }   
 })
 
+// Display update book form
+router.get('/:id' , (req,res) => Book.findAll()
+        .then( (books) => {   
+            let foundID = false;
+            books.forEach( (book) => {
+                if (book.id == req.params.id)
+                {
+                    foundID = true;
+                    chosenBook = book;
+                }
+                    
+            });
+            if (foundID)
+                res.render('update-book' , {chosenBook}); 
+            else
+                res.render('page-not-found');    
+        })
+        .catch((err) => console.log(err))
+)
+
+// Update book
+router.post('/:id', (req,res) => {
+    let { title, author, genre, year} = req.body;
+    let errors = [];
+
+    // Validations of title and author
+    if (!title)
+        errors.push({text: 'Please add a Title'});
+    if (!author)
+        errors.push({text: 'Please add an Author'});
+
+    // Check for errors
+    if(errors.length > 0)
+    {
+        res.render('update-book' , {
+            errors,
+            title,
+            author,
+            genre,
+            year
+        });
+    }   
+    else
+    {
+        Book.findByPk(req.params.id)
+            .then( (book) => book.update(req.body))
+            .then( () => res.redirect('/books'))
+            .catch( (err) => console.log(err) )
+    }
+})
 
 module.exports = router;
